d258002d4e51a90c4e443d00b0dc7e614c9e6430
birtek <barszy94@gmail.com>
2020-06-07 14:06:30 +0200

    Add unit Test to method getEmployee() in EmployeeController.

---
+++ src/test/java/pl/szydlowski/springbootcrudapi/controller/EmployeeControllerTest.java
@@ -0,0 +1,48 @@
+package pl.szydlowski.springbootcrudapi.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import pl.szydlowski.springbootcrudapi.model.Employee;
+import pl.szydlowski.springbootcrudapi.service.EmployeeServiceImpl;
+
+import java.sql.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+class EmployeeControllerTest {
+
+    @InjectMocks
+    EmployeeController employeeController;
+
+    @Mock
+    EmployeeServiceImpl employeeService;
+
+    Employee employee;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        employee = new Employee();
+        employee.setId(1);
+        employee.setDate(Date.valueOf("1994-11-12"));
+        employee.setDepartment("hr");
+        employee.setGender("male");
+        employee.setName("Alex");
+    }
+
+    @Test
+    void getEmployee() {
+        when(employeeService.getEmployeeById(anyInt())).thenReturn(employee);
+
+        Employee employeeRest = employeeController.getEmployee(77);
+
+        assertNotNull(employeeRest);
+        assertEquals(employee.getName(),employeeRest.getName());
+    }
+}
